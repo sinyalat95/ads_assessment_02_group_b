fb7296fbb0275a7bfc8445965aa93f018df88e77
sinyalat95 <ashganmusa@windowslive.com>
2024-04-30 11:15:30 +0200

    Submit assessment

--- main.py
+++ main.py
@@ -2,7 +2,13 @@
 Multiplication Table
 """
 # Provide your solution here
+def print_multiplication_table(a, b) -> int:
+    while b > 0:
+        b -= 1
+        print(a * b)
 
+print_multiplication_table(6, 3)
+print_multiplication_table(5, 5)
 """
 Palindromes
 """
@@ -11,5 +17,5 @@ Palindromes
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print("Test your functions by calling them here. Use different parameter values to test them with different scenarios.")
-
-
+# def print_multiplication_table(a, b) -> int:
+#
